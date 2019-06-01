4f895fc2329a01a4c71ca2ac8b68eef45dbf66b5
aaronk6 <aaronk6@users.noreply.github.com>
2019-06-01 16:56:05 +0200

    Added TRON Power and TRC10 tokens

--- TRON.lua
+++ TRON.lua
@@ -25,7 +25,7 @@
 -- SOFTWARE.
 
 WebBanking{
-  version = 1.00,
+  version = 1.01,
   description = "Fetches balances from tronscan.org and returns them as securities",
   services = { "TRON" },
 }
@@ -35,7 +35,9 @@ local currency = "EUR" -- fixme: Don't hardcode
 local currencyField = "price_eur"
 local marketName = "CoinMarketCap"
 local priceUrl = "https://api.coinmarketcap.com/v1/ticker/tron/?convert=EUR"
-local balanceUrl = "https://apilist.tronscan.org/api/account?address="
+local accountUrl = "https://apilist.tronscan.org/api/account?address="
+local tokenUrl = "https://apilist.tronscan.org/api/token?showAll=1&id="
+local tokenNames = {}
 
 local addresses
 local balances
@@ -62,17 +64,30 @@ end
 
 function RefreshAccount (account, since)
   local s = {}
-  local prices = queryPrices()
-  local balances = queryBalances(addresses)
+  local t
+  local trx_price = queryPrices()[currencyField]
+  local accounts = queryAccounts(addresses)
+  local account
 
   for i,v in ipairs(addresses) do
-    s[i] = {
-      name = v,
-      currency = nil,
-      market = marketName,
-      quantity = balances[i] / 1000000,
-      price = prices[currencyField],
-    }
+    account = accounts[i]
+
+    -- TRX
+    s[#s+1] = createSecurity(account["address"], "TRX", "TRX", account["balance"], 6, trx_price)
+
+    -- TRON Power (TP)
+    s[#s+1] = createSecurity(account["address"], "TRON Power", "TP", calculateTronPower(account), 6, trx_price)
+
+    -- TRC10 Tokens
+    for key, token in pairs(accounts[i]["balances"]) do
+      -- "_" is for TRX, we've already handled it above and can ignore it here.
+      if token["name"] ~= "_" then
+        t = resolveToken(token)
+        if t then
+          s[#s+1] = createSecurity(account["address"], t["name"], t["symbol"], t["balance"], t["decimals"], t["priceInTrx"] * trx_price)
+        end
+      end
+    end
   end
 
   return {securities = s}
@@ -81,23 +96,84 @@ end
 function EndSession ()
 end
 
+function resolveToken(token)
+  local id = token["name"]
+
+  -- return from cache
+  if tokenNames[id] ~= nil then return tokenNames[id] end
+
+  -- "_" is for TRX (not a token we can query)
+  if id == "_" then return nil end
+
+  -- ignore tokens that don't have a TRX price
+  if not token["priceInTrx"] then return nil end
+
+  local connection = Connection()
+  local res = JSON(connection:request("GET", tokenUrl .. id)):dictionary()["data"][1]
+
+  tokenNames[id] = {
+    name = res["name"],
+    symbol = res["abbr"],
+    balance = token["balance"],
+    priceInTrx = token["priceInTrx"],
+    decimals = res["precision"]
+  }
+
+  return tokenNames[id]
+end
+
+function calculateTronPower(account)
+  local balance = account["frozen"]["total"]
+
+  -- The following is required to get the correct balance during the fixed frozen period (3 days after freezing).
+
+  local ar = account["accountResource"]
+
+  if ar["frozen_balance_for_energy"] and ar["frozen_balance_for_energy"]["frozen_balance"] then
+    balance = balance + ar["frozen_balance_for_energy"]["frozen_balance"]
+  end
+
+  if ar["frozen_balance_for_bandwidth"] and ar["frozen_balance_for_bandwidth"]["frozen_balance"] then
+    balance = balance + ar["frozen_balance_for_bandwidth"]["frozen_balance"]
+  end
+
+  return balance
+end
+
+function createSecurity(address, name, symbol, balance, decimals, price)
+
+  local description = ""
+
+  if symbol ~= name then
+    description = " (" .. name .. ")"
+  end
+
+  return {
+    name = symbol .. description .. " · " .. address,
+    currency = nil,
+    market = marketName,
+    quantity = balance / math.pow(10, decimals),
+    price = price
+  }
+end
+
 function queryPrices()
   local connection = Connection()
   local res = JSON(connection:request("GET", priceUrl))
   return res:dictionary()[1]
 end
 
-function queryBalances(addresses)
+function queryAccounts(addresses)
   local connection = Connection()
-  local balances = {}
+  local accounts = {}
   local res
 
   for key, address in pairs(addresses) do
-    res = JSON(connection:request("GET", balanceUrl .. address))
-    table.insert(balances, res:dictionary()["balance"])
+    res = JSON(connection:request("GET", accountUrl .. address)):dictionary()
+    table.insert(accounts, res)
   end
 
-  return balances
+  return accounts
 end
 
 -- from http://lua-users.org/wiki/SplitJoin
