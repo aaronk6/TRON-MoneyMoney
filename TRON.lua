c8b68bf17ad522e57c5982214992f2022fa25888
aaronk6 <aaronk6@users.noreply.github.com>
2020-03-13 15:28:27 +0100

    Copyright 2020

--- TRON.lua
+++ TRON.lua
@@ -4,7 +4,7 @@
 -- Username: 76cf8a94..., fdea5932...
 -- Password: (anything)
 --
--- Copyright (c) 2019 aaronk6
+-- Copyright (c) 2020 aaronk6
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
